34ae1dce4fca179c65506870787caf0ca647ce21
timlidowin <tim@lidowin.com>
2023-09-13 05:39:46 +0700

    add token validation

--- src/adapters/api/shared/mod.rs
+++ src/adapters/api/shared/mod.rs
@@ -5,3 +5,4 @@ pub mod response;
 pub mod enum_response;
 pub mod init_global;
 pub mod request_be;
+pub mod validate_token;
--- src/adapters/api/spin_reward/spin_reward_controllers.rs
+++ src/adapters/api/spin_reward/spin_reward_controllers.rs
@@ -1,5 +1,5 @@
 use actix_web::{ web::{self, Json}, HttpResponse,post,Result, get, HttpRequest};
-use crate::{adapters::api::{shared::{app_state::AppState, response::GenericResponse, error_presenter::ErrorReponse}, spin_reward::{spin_reward_payload::{SpinRewardPayload, SpinRewardUpdatedPayload}, spin_reward_presenters::SpinRewardsPresenter, spin_reward_mappers::SpinRewardPresenterMapper, query_string::QstringReward}}, 
+use crate::{adapters::api::{shared::{app_state::AppState, response::{GenericResponse, JwtResponse}, validate_token::check_validation}, spin_reward::{spin_reward_payload::{SpinRewardPayload, SpinRewardUpdatedPayload}, spin_reward_presenters::SpinRewardsPresenter, spin_reward_mappers::SpinRewardPresenterMapper, query_string::QstringReward}}, 
 application::{usecases::{spin_rewards::{post_spin_rewards::PostSpinRewardsUseCase, list_spin_rewards::ListSpinRewardsUseCase, update_spin_rewards::UpdateSpinRewardsUseCase}, interfaces::AbstractUseCase}, mappers::api_mapper::ApiMapper}, 
 domain::error::ApiError};
 
@@ -11,32 +11,76 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
 }
 
 #[post("/store")]
-async fn post_spin_rewards(data: web::Data<AppState>,post:Json<SpinRewardPayload>) ->Result<HttpResponse,ErrorReponse> {
+async fn post_spin_rewards(data: web::Data<AppState>,post:Json<SpinRewardPayload>,req: HttpRequest) ->HttpResponse {
     let spin_reward = PostSpinRewardsUseCase::new(&post, &data.connection_repository);
     let spin_rewards: Result<GenericResponse, ApiError> = spin_reward.execute().await;
-    spin_rewards
-    .map_err(ErrorReponse::map_io_error)
-    .map(|response| HttpResponse::Ok().json(response))
+    let header_authorization =  req.headers().get("Authorization");
+    if header_authorization.is_none(){
+        let error = JwtResponse{
+            message: "Empty Bearer Authorization !!".to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    let auth = header_authorization.unwrap().to_str().ok().unwrap().to_string(); 
+    let jwt_token_company_code = check_validation(auth);
+    if jwt_token_company_code.contains("Error"){
+        let error = JwtResponse{
+            message: jwt_token_company_code.to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    return HttpResponse::Ok().json(spin_rewards.unwrap());
 }
 
-#[get("/list/{company_code}")]
-async fn get_all_spin_rewards(data: web::Data<AppState>,path:web::Path<(String,)>,req: HttpRequest) ->Result<HttpResponse,ErrorReponse> {
+#[get("/list")]
+async fn get_all_spin_rewards(data: web::Data<AppState>,req: HttpRequest) ->HttpResponse {
     let qstring = web::Query::<QstringReward>::from_query(req.query_string()).unwrap();
-  
-    let company_code = path.into_inner().0.to_string();
+    let header_authorization =  req.headers().get("Authorization");
+    if header_authorization.is_none(){
+        let error = JwtResponse{
+            message: "Empty Bearer Authorization !!".to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    let auth = header_authorization.unwrap().to_str().ok().unwrap().to_string(); 
+    let jwt_token_company_code = check_validation(auth);
+    if jwt_token_company_code.contains("Error"){
+        let error = JwtResponse{
+            message: jwt_token_company_code.to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    let company_code = jwt_token_company_code.to_string();
     let spin_reward = ListSpinRewardsUseCase::new(&company_code,&qstring,&data.connection_repository);
     let spin_reward: std::result::Result<Vec<crate::domain::spin_reward_entity::SpinRewardEntity>, ApiError> = spin_reward.execute().await;
+    HttpResponse::Ok().json(spin_reward.unwrap().into_iter().map(SpinRewardPresenterMapper::to_api).collect::<Vec<SpinRewardsPresenter>>())
     
-    spin_reward
-        .map_err(ErrorReponse::map_io_error)
-        .map(|data| HttpResponse::Ok().json(data.into_iter().map(SpinRewardPresenterMapper::to_api).collect::<Vec<SpinRewardsPresenter>>()))
 }
 
 #[post("/update")]
-async fn update_spin_rewards(data: web::Data<AppState>,post:Json<SpinRewardUpdatedPayload>) ->Result<HttpResponse,ErrorReponse> {
+async fn update_spin_rewards(data: web::Data<AppState>,post:Json<SpinRewardUpdatedPayload>,req: HttpRequest) ->HttpResponse {
     let spin_reward = UpdateSpinRewardsUseCase::new(&post, &data.connection_repository);
     let spin_rewards: Result<GenericResponse, ApiError> = spin_reward.execute().await;
-    spin_rewards
-    .map_err(ErrorReponse::map_io_error)
-    .map(|response| HttpResponse::Ok().json(response))
+    let header_authorization =  req.headers().get("Authorization");
+    if header_authorization.is_none(){
+        let error = JwtResponse{
+            message: "Empty Bearer Authorization !!".to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    let auth = header_authorization.unwrap().to_str().ok().unwrap().to_string(); 
+    let jwt_token_company_code = check_validation(auth);
+    if jwt_token_company_code.contains("Error"){
+        let error = JwtResponse{
+            message: jwt_token_company_code.to_string(),
+            status:  "error".to_string()
+        };
+       return HttpResponse::Ok().json(error);
+    }
+    return HttpResponse::Ok().json(spin_rewards.unwrap());
 }
