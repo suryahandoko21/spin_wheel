e09ff47fbbea001d605b3176144722ad1c9d4761
timlidowin <tim@lidowin.com>
2023-09-19 14:22:59 +0700

    add token validation request BE

--- src/adapters/api/shared/init_global.rs
+++ src/adapters/api/shared/init_global.rs
@@ -8,12 +8,16 @@ pub fn set_global_init(){
     let database_name = dotenv::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
     let limit_spin = dotenv::var("LIMIT_SPIN").expect("LIMIT_SPIN must be set");
     let url_limit_be = dotenv::var("URL_LIMIT_SPIN_BE").expect("URL_LIMIT_SPIN_BE must be set");
+    let token_validation_be = dotenv::var("TOKEN_VALIDATION_BE").expect("TOKEN_VALIDATION_BE must be set");
+    let enable_token_validation = dotenv::var("ENABLE_TOKEN_VALIDATION").expect("ENABLE_TOKEN_VALIDATION must be set");
     
     let _ = GLOBAL_INIT.set([
         ("database_url".to_string(), database_url),
         ("webhook_be".to_string(), webhook_be),
         ("database_name".to_string(), database_name),
         ("limit_spin".to_string(),limit_spin),
-        ("url_spin_limit_be".to_string(),url_limit_be)
+        ("url_spin_limit_be".to_string(),url_limit_be),
+        ("token_validation_be".to_string(),token_validation_be),
+        ("enable_token_validation".to_string(),enable_token_validation)  
         ].into());
 }
--- src/adapters/spi/spintickets/request_limit_spin.rs
+++ src/adapters/spi/spintickets/request_limit_spin.rs
@@ -7,10 +7,12 @@ pub async fn get_request_limit(uuid:&mut String)->bool{
     };
     let global_map = GLOBAL_INIT.get().unwrap();
     let url_spin_limit_be = &global_map["url_spin_limit_be"];
+    let token_validation_be = &global_map["token_validation_be"];
     let limit_spin = &global_map["limit_spin"].parse().unwrap_or(false);
     if *limit_spin{
         let client = awc::Client::default();
         let res = client.get(url_spin_limit_be)
+            .insert_header(("spinWheelEngineSecretKey", token_validation_be.to_string()))
             .send_json(&req_limit)
             .await;
         let rstatus =  res.as_ref();
@@ -18,6 +20,7 @@ pub async fn get_request_limit(uuid:&mut String)->bool{
         if status == 200 {
             let body = res.ok().unwrap().body().await.ok();
             let rs:ResponseLimitSpin = serde_json::from_slice(&body.unwrap()).unwrap();
+            println!("response get{:?}",rs);
             if rs.status == "NOT_AVAILABLE"{
               return false;
             }  
