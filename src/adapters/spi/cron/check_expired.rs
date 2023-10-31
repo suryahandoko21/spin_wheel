766383e14b62094baa80e327e8562908796f6ce6
timlidowin <tim@lidowin.com>
2023-10-31 15:04:51 +0700

    update cron expire ticket

--- src/adapters/spi/cron/check_expired.rs
+++ src/adapters/spi/cron/check_expired.rs
@@ -1,7 +1,7 @@
 use crate::adapters::spi::cfg::pg_connection::CONN;
 use crate::adapters::spi::cfg::schema::tb_spin_tickets::dsl::*;
 use crate::adapters::spi::spintickets::models::SpinTickets;
-use chrono::{NaiveDate, Utc};
+use chrono::{Local, NaiveDateTime};
 use diesel::prelude::*;
 pub async fn check_ticket_expired_be() {
     let result: Result<Vec<SpinTickets>, _> =
@@ -10,15 +10,12 @@ pub async fn check_ticket_expired_be() {
             .load::<SpinTickets>(&mut CONN.get().unwrap().get().expect("cant connect database"));
     for i in result.iter() {
         for data in i.iter() {
-            let ex_value = data.expired_value;
             let t_uuid = data.ticket_uuid.to_string();
-            let d_now = Utc::now().date_naive();
-            let created = data.created_date.to_string();
-            let d_created = created.split(" ").next().unwrap();
-            let n_date = NaiveDate::parse_from_str(&d_created, "%Y-%m-%d").unwrap();
-            let d_diff = d_now.signed_duration_since(n_date);
-            let days = d_diff.num_days();
-            if days >= ex_value.into() {
+            let now = Local::now().naive_local();
+            let expired = data.expired_date.to_string();
+            let format_str = "%Y-%m-%d %H:%M:%S";
+            let expire_date = NaiveDateTime::parse_from_str(&expired, format_str).unwrap();
+            if expire_date <= now {
                 let _expired_used =
                     diesel::update(tb_spin_tickets.filter(ticket_uuid.eq(t_uuid.to_string())))
                         .set(status.eq("EXPIRED"))
